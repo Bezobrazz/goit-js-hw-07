46399dbcdde4af95fc8955977c466c86ca5dfc47
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2023-11-29 20:44:36 +0200

    finished 02

--- src /js/02-lightbox.js
+++ src /js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryElem = document.querySelector(".gallery");
+
+galleryElem.addEventListener("click", onGalleryElemClick);
+
+function onGalleryElemClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) return;
+}
+
+renderGallery();
+
+function generateGalleryItemMarkup(item) {
+  return `
+	<li class="gallery__item">
+		<a class="gallery__link" href="${item.original}">
+			<img class="gallery__image" src="${item.original}" alt="${item.description}" />
+		</a>
+	</li>`;
+}
+
+function generateGalleryMarkup(items) {
+  return items.map(generateGalleryItemMarkup).join("");
+}
+
+function renderGallery() {
+  const galleryMarkup = generateGalleryMarkup(galleryItems);
+  galleryElem.innerHTML = galleryMarkup;
+
+  // SimpleLightbox
+  const lightbox = new SimpleLightbox(".gallery a", {});
+}
