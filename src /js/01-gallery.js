51cc1977f26708111a07f243d7c452c97323b8a4
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2023-11-29 19:53:36 +0200

    finished 01

--- src /js/01-gallery.js
+++ src /js/01-gallery.js
@@ -4,6 +4,32 @@ console.log(galleryItems);
 
 const galleryElem = document.querySelector(".gallery");
 
+galleryElem.addEventListener("click", onGalleryElemClick);
+
+function onGalleryElemClick(e) {
+  e.preventDefault();
+  if (e.target === e.currentTarget) return;
+  console.log(e);
+
+  const imageSrc = e.target.dataset.source;
+  const imageAlt = e.target.alt;
+
+  const instance = basicLightbox.create(`
+    <img width="800" height="600" src="${imageSrc}" alt="${imageAlt}">
+  `);
+
+  instance.show();
+
+  window.addEventListener("keydown", onEscapePress);
+
+  function onEscapePress(e) {
+    if (e.key === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscapePress);
+    }
+  }
+}
+
 renderGallery();
 
 function generateGalleryItemMarkup(item) {
